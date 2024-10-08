54d000446463a1bbfcde1a25c2008606e64914ac
Arthur Kanev <artur_kholod_97@mail.ru>
2024-10-08 14:44:01 +0300

    Initial commit for mymath v1.0.0

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ArthurGopher/mymath
+
+go 1.19
---
+++ main.go
@@ -0,0 +1,23 @@
+package mymath
+
+import "math"
+
+func Abs(x float64) float64 {
+	return math.Abs(x)
+}
+
+func Acos(x float64) float64 {
+	return math.Acos(x)
+}
+
+func Acosh(x float64) float64 {
+	return math.Acosh(x)
+}
+
+func Asin(x float64) float64 {
+	return math.Asin(x)
+}
+
+func Asinh(x float64) float64 {
+	return math.Asinh(x)
+}
